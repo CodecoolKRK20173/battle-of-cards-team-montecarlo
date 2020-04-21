a0a2fccbdaf6a47c69c830908073c17d57272533
Marta Skarbek <marta@skarbek.name>
2020-04-21 12:50:39 +0200

    added class for get input from user

---
+++ src/main/java/com/myapp/battleofcards/InputProvider.java
@@ -0,0 +1,10 @@
+package com.myapp.battleofcards;
+
+import java.util.Scanner;
+
+public class InputProvider {
+
+    public void intInput(String message) {
+        Scanner scanner = new Scanner(message);
+    }
+}
