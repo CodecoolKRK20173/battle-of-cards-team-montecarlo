36a8db4129f98b42e002c10dea7744ad7b1d08c7
pirat77 <mcyanek@gmail.com>
2020-04-21 12:09:28 +0200

    Creates structure cards.xml containing basic game pieces

--- src/main/java/com/myapp/battleofcards/Main.java
+++ src/main/java/com/myapp/battleofcards/Main.java
@@ -3,5 +3,7 @@ package com.myapp.battleofcards;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+        int numberOfPlayers = 2;
+        new Game(numberOfPlayers);
     }
 }
