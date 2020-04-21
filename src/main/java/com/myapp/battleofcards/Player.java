92f682f7c698f998eecb5de4f7b4ac00dc66d0d0
Marta Skarbek <marta@skarbek.name>
2020-04-21 14:24:46 +0200

    changes for mor readable of code

--- src/main/java/com/myapp/battleofcards/Player.java
+++ src/main/java/com/myapp/battleofcards/Player.java
@@ -4,13 +4,17 @@ import java.util.LinkedList;
 
 public abstract class Player{
     private LinkedList<Card> cards;
+
     Player(){
         cards = new LinkedList<Card>();}
+
     void putAtBottom(Card card){
         this.cards.add(0, card);
     }
+
     public Card drawNext(){
         return this.cards.removeLast();
     }
+
     public abstract void chooseStat();
 }
