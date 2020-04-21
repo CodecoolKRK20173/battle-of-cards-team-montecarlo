07abdef72476f06feda78b234347ec8dab1139eb
Marta Skarbek <marta@skarbek.name>
2020-04-21 11:04:15 +0200

    addex extended class for player

--- src/main/java/com/myapp/battleofcards/Human.java
+++ src/main/java/com/myapp/battleofcards/Human.java
@@ -3,6 +3,8 @@ package com.myapp.battleofcards;
 import java.util.LinkedList;
 
 public class Human extends Player {
+    private LinkedList<Card> cards;
+
         Human (){
             super();
         }
