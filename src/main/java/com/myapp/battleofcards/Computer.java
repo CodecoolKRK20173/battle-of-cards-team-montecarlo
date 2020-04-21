301d600ba4c89cd10a95b032ada66aa005863cba
Marta Skarbek <marta@skarbek.name>
2020-04-21 11:04:00 +0200

    addex extended class for player

--- src/main/java/com/myapp/battleofcards/Computer.java
+++ src/main/java/com/myapp/battleofcards/Computer.java
@@ -1,6 +1,10 @@
 package com.myapp.battleofcards;
 
+import java.util.LinkedList;
+
 public class Computer extends Player {
+    private LinkedList<Card> cards;
+
     Computer (){
         super();
     }
