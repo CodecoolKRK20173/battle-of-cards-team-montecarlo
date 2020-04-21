599b3e8365f1aa5a2a4df772ba3d1e0ef44acadd
Marta Skarbek <marta@skarbek.name>
2020-04-21 10:27:12 +0200

    added class extends player

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,4 +1,16 @@
 package player;
 
-public class Player {
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class Player {
+    private LinkedList<Card> playerStack;
+
+    public void drawNext() {
+         //prints to table next card involved to game
+    }
+
+    public void putAtBottom() {
+        //put gotted card to bottomgit
+    }
 }
